3b802ccf91f4d87ad79b43d9a58473b292da53c9
Nesma <nesma.megid@gmail.com>
2017-03-18 06:13:43 +0200

    Create app.module.js

---
+++ app/app.module.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular.module('app', [
+        'ngAria',
+        'ngAnimate',
+        'ngMaterial', 
+        'app.core',
+        'app.login',
+        'app.menu'
+    ]);
+
+})();
